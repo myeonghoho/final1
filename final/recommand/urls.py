25885bdf08228ba9f60dd88d7da2781c2138ecf9
DESKTOP-ONE4R81\ITSC <r5e@naver.com>
2024-05-21 12:41:41 +0900

    5/21 오전 커밋
    - m_ame 상세페이지 사진 정렬 완성

--- final/recommand/urls.py
+++ final/recommand/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path("man/", views.man, name = "man"),  
     path("woman/", views.woman, name = "woman"),  
     path("upload/", views.upload, name = "upload"),
-    path("m_ame/", views.m_ame, name= "m_ame"),
+    path("m_ame_1/", views.m_ame_1, name= "m_ame_1"),
+    path("m_ame_2/", views.m_ame_2, name= "m_ame_2"),
+    path("m_ame_3/", views.m_ame_3, name= "m_ame_3"),
+    path("m_ame_4/", views.m_ame_4, name= "m_ame_4"),
+    path("m_ame_5/", views.m_ame_5, name= "m_ame_5"),
+
 ]
--- final/recommand/views.py
+++ final/recommand/views.py
@@ -19,6 +19,17 @@ def woman(request):
 def upload(request):
     return render(request, "upload.html")
 
-def m_ame(request):
-    return render(request, "man/man_detail/m_americancasual.html")
+def m_ame_1(request):
+    return render(request, "man/man_detail/m_ame/m_americancasual.html")
 
+def m_ame_2(request):
+    return render(request, "man/man_detail/m_ame/m_americancasual_2.html")
+
+def m_ame_3(request):
+    return render(request, "man/man_detail/m_ame/m_americancasual_3.html")
+
+def m_ame_4(request):
+    return render(request, "man/man_detail/m_ame/m_americancasual_4.html")
+
+def m_ame_5(request):
+    return render(request, "man/man_detail/m_ame/m_americancasual_5.html")
